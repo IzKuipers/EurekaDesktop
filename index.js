178c84003bfebf69230d65e6920e4816dfc168aa
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-10-22 10:24:49 +0200

    Create project

---
+++ index.js
@@ -0,0 +1,27 @@
+import { app, BrowserWindow, dialog } from "electron";
+
+app.on("ready", () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 640,
+    minHeight: 480,
+    title: "EUREKA",
+    backgroundColor: "#fff",
+    devTools: true,
+  });
+
+  win.removeMenu();
+  win.loadURL("https://eureka.izkuipers.nl/");
+
+  win.addListener("close", (e) => {
+    const response = dialog.showMessageBoxSync(win, {
+      type: "question",
+      title: "Exit Eureka?",
+      message: "Are you sure you want to exit?",
+      buttons: ["Yes", "No"],
+    });
+
+    if (response === 1) e.preventDefault();
+  });
+});
